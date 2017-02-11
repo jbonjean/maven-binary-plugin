ca3311d524f583e85812d2825120d393fffef256
Julien Bonjean <julien.bonjean@adenops.com>
2017-02-10 19:16:51 -0500

    add support for JVM options

--- src/main/resources/wrapper.sh
+++ src/main/resources/wrapper.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 target="/tmp/$(basename $0).jar"
 trap "rm -f '$target'" INT
-tail -n +8 "$0" > "$target"
-java JAVA_OPTIONS -jar "$target" "$@"
+tail -n +20 "$0" > "$target"
+JVM_OPTIONS=${JVM_OPTIONS:-}
+while [ $# -ne 0 ]; do
+	case $1 in
+		-D*|-X*)
+			JVM_OPTIONS="$JVM_OPTIONS $1"
+			shift
+			;;
+		*)
+			break
+			;;
+	esac
+done
+java JAVA_OPTIONS $JVM_OPTIONS -jar "$target" "$@"
 rm -f "$target"
 exit
